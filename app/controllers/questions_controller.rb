2089b41e1f3fb41928f749d8591564d06d6443b3
miyakooti <psychic.spoon313@gmail.com>
2020-09-18 05:05:32 +0000

    レイアウト修正　9/18

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,10 +13,28 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
   
+  def edit
+    @question = Question.find(params[:id])
+    @button_value = "保存する"
+
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+    redirect_to root_path, notice:"編集が完了しました！"
+  end
+  
   def create
     Question.create(quiz: question_params[:quiz], answer: question_params[:answer], user_id: current_user.id)
     #なななんと、フォーム側でuser_id指定しなくてもいい
-    redirect_to root_path
+    redirect_to root_path, notice:"保存が完了しました！"
+  end
+  
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path, notice:"削除しました。"
   end
   
   private
