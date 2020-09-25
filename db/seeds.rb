6821f9382bd7082e6a1d5cd2f0664d54d1f78733
miyakooti <psychic.spoon313@gmail.com>
2020-09-25 13:02:14 +0000

    管理ユーザの初期データファイル(seed)を、本番環境でも実行できるようにする。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-Question.create!(quiz: "日本一高い山", answer: "富士山", user_id: 1)
-Question.create!(quiz: "日本3高い山", answer: "しらん", user_id: 1)
-Question.create!(quiz: "日本２高い山", answer: "北岳", user_id: 1)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
